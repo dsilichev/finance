ddef4ccc6ea64654a1569469fbac01167f2f213c
Dmitry <dsilichev@gmail.com>
2024-09-10 17:17:45 +0300

    account routes

--- routes/account.js
+++ routes/account.js
@@ -7,9 +7,9 @@ const {
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {});
+//router.get("/", async (req, res) => {});
 
-router.get("/:id", async (req, res) => {});
+//router.get("/:id", async (req, res) => {});
 
 router.post("/", async (req, res) => {
   await addAccount({
@@ -19,8 +19,15 @@ router.post("/", async (req, res) => {
   });
 });
 
-router.post("/:id", async (req, res) => {});
+router.patch("/:id", async (req, res) => {
+  const updatedAccount = await editAccount(req.params.id, {
+    title: req.body.title,
+    currency: req.body.currency,
+  });
+});
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  await deleteAccount(req.params.id);
+});
 
 module.exports = router;
