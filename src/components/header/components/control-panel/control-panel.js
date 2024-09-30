158f10e9fa134f44d85b5dad983ea3781f68b073
Dmitry <dsilichev@gmail.com>
2024-09-30 17:14:25 +0300

    save progress

---
+++ src/components/header/components/control-panel/control-panel.js
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+const ControlPanelContainer = ({ className }) => {
+  // login logout pic
+  return <div className={className}></div>;
+};
+
+export const ControlPanel = styled(ControlPanelContainer)`
+
+`;
