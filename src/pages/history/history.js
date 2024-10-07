66af34bda7838122905ef94e368fde4b3a1b93c5
Dmitry <dsilichev@gmail.com>
2024-10-07 17:40:45 +0300

    request for history page

--- src/pages/history/history.js
+++ src/pages/history/history.js
@@ -1,5 +1,31 @@
-export const History = () => {
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { request } from '../../utils';
+import { PAGINATION_LIMIT } from '../../constants';
+
+const HistoryContainer = ({className}) => {
+
+  const [transactions, setTransactions] = useState([]);
+  const [account, setAccount] = useState('');
+  const [category, setCategory] = useState('');
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+
+  useEffect(() => {
+    request(`/api/transactions?account=${account}&category=${category}&page=${page}&limit=${PAGINATION_LIMIT}`, ).then(({data: {transactions, lastPage}}) => {
+      setTransactions(transactions);
+      
+      setLastPage(lastPage);
+    });
+  }, [account, category, page])
+
+  console.log(transactions)
+
   return (
-    <div>History</div>
+    <div className={className}>{lastPage}</div>
   )
 }
+
+export const History = styled(HistoryContainer)`
+
+`
